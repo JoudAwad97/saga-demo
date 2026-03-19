6cf62a9d9e7d94ad8942114073b96fba5ea979b9
Joud Awad <thejoud1997@gmail.com>
2026-03-20 00:10:10 +0300

    remove dist folder

--- dist/generated/prisma/browser.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Prisma from './internal/prismaNamespaceBrowser.js';
-export { Prisma };
-export * as $Enums from './enums.js';
-export * from './enums.js';
-export type Consumer = Prisma.ConsumerModel;
-export type Order = Prisma.OrderModel;
-export type Ticket = Prisma.TicketModel;
-export type CreditCardAuthorization = Prisma.CreditCardAuthorizationModel;
--- dist/generated/prisma/models.d.ts
+++
@@ -1,5 +0,0 @@
-export type * from './models/Consumer.js';
-export type * from './models/Order.js';
-export type * from './models/Ticket.js';
-export type * from './models/CreditCardAuthorization.js';
-export type * from './commonInputTypes.js';
